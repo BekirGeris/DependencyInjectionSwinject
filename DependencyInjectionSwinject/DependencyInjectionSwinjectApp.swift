d3dbec69aae2402b8550b0d875dd10b67fd35af8
BekirGeris <48486272+BekirGeris@users.noreply.github.com>
2023-10-04 03:04:34 +0300

    Initial Commit

---
+++ DependencyInjectionSwinject/DependencyInjectionSwinjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyInjectionSwinjectApp.swift
+//  DependencyInjectionSwinject
+//
+//  Created by Bekir Geriş on 4.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct DependencyInjectionSwinjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
